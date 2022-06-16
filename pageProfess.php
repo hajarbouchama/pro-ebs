0952317bee16f6c244add965317e44fde553e9d1
hajarbouchama <bouchamahajar3@gmail.com>
2022-06-16 19:28:21 +0100

    php

--- pageProfess.php
+++ pageProfess.php
@@ -1,19 +1,4 @@
-<?php 
-$user = "root";
-$pass = "";
-$db = "bd_pro";
-$serv="localhost";
-
-    $BD = mysqli_connect($serv,$user,$pass,$db); 
-
-
-?>
-
-
-
-
-
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -23,21 +8,20 @@ $serv="localhost";
 	<title>ebs</title>
 	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 	<link rel="stylesheet" href="./css/style.css">
-	<link rel="stylesheet" href="./css/pageEtud.css">
-	<link rel="stylesheet" href="./css/swiper-bundle.min.css">
-	<!--<link rel="stylesheet" href="./css/courses-style.css">-->
+	<link rel="stylesheet" href="./css/pageProfess.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel="icon" type="image/x-icon" href="./images/fav__icon.svg">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
-	<navbar>
+	 <!--------------------------------------------------------------------------NAVBAR------------------------------------------------------------>
+	 <navbar>
 		<div class="container nav__container">
 			<a href="index.html"><img src="images/logo.svg"></a>
 			<ul class="nav__menu">
-				<li><a href="pageEtud.php">Notre Service</a></li>
-				<li><a href="pageProfess.php">Equipe</a></li>
+				<li><a href="index.html">Notre Service</a></li>
+				<li><a href="index.html">Equipe</a></li>
 				<li><a href="index.html">FAQ</a></li>
 				<li><a href="index.html">Contact</a></li>
 				<li><a href="index.html">s'inscrir</a></li>
@@ -46,67 +30,27 @@ $serv="localhost";
 			<button id="close-menu-btn"><i class="uil uil-multiply"></i></button>
 		</div>
 	</navbar>
+	
+	<!--------------------------------------------------------------------------form------------------------------------------------------------>
+	
+	<section class="upload">
+		<h1>Uploader un fichier PDF :</h1>
+		<div class="container upload__container">
 
-
-	<!------------------------------CAI------------------------------------------->
-
-		<div class="container ">
-			<h1>CAI :</h1>
-			
-			<div class="slide-container swiper">
-				<div class="slide-content">
-					<div class="card-wrapper swiper-wrapper">
-						<arcticle class="course card swiper-slide">
-							<div class="course__image image-content">
-								<img src="./images/course1.jpg">
-							</div>
-							<div class="course__info">
-							  <h4>Responsive website</h4>
-							  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque assumenda adipisci, quidem quisquam molestiae repellendus.</p>
-							  <a href="course.html" target="_blank"class="btn btn-primary">learn more</a>
-							</div>
-							
-						</arcticle>
-
-						<arcticle class="course card swiper-slide">
-							<div class="course__image image-content">
-								<img src="./images/course1.jpg">
-							</div>
-							<div class="course__info">
-							  <h4>Responsive website</h4>
-							  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque assumenda adipisci, quidem quisquam molestiae repellendus.</p>
-							  <a href="course.html" target="_blank"class="btn btn-primary">learn more</a>
-							</div>
-							
-						</arcticle>
-
-						<arcticle class="course card swiper-slide">
-							<div class="course__image image-content">
-								<img src="./images/course1.jpg">
-							</div>
-							<div class="course__info">
-							  <h4>Responsive website</h4>
-							  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque assumenda adipisci, quidem quisquam molestiae repellendus.</p>
-							  <a href="course.html" target="_blank"class="btn btn-primary">learn more</a>
-							</div>
-							
-						</arcticle>
-						
-
-					</div>
-				</div>
-		
-				<div class="swiper-button-next swiper-navBtn"></div>
-				<div class="swiper-button-prev swiper-navBtn"></div>
-				<div class="swiper-pagination"></div>
-			</div>
-
+			<form class="upload__form" method="post" enctype="multipart/form-data">
+				<label for="title">titre de coures</label>
+				<input type="text" placeholder="titre de cours" required>
+				<label for="description">description</label>
+				<input type="text" rows="10" placeholder="description">
+				<label for="fname">cours en PDF</label>
+				<input type="file" name="fichier" required>
+				<button type="submit" class="btn btn-primary">Enregister le fichier</button>
+			</form>
 		</div>
-	<!------------------------------DAI------------------------------------------->
-		
-<!--faq-->
+	</section>
+	
 
-
+	<!------------------------------FAQ------------------------------------------->
 	<section class="faqs">
 		<h2>Frequently asked questions</h2>
 		<div class="container faqs__container">
@@ -168,14 +112,6 @@ $serv="localhost";
 		</article>
 		</div>
 	</section>
-
-
-
 	
 </body>
-<!-- Swiper JS -->
-<script src="js/swiper-bundle.min.js"></script>
-
-<!-- JavaScript -->
-<script src="js/script.js"></script>
 </html>
